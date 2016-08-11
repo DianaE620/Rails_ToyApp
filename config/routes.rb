2a398016417e22aaa74ad4dabe3b1701d930f6e3
codeamac07 <codeamac07@gmail.com>
2016-08-11 17:16:13 -0500

    Crud usuarios y microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  # Toda la base de los micropost se hizo solo ejecutando rails generate scaffold Micropost content:text user_id:integer
+  resources :microposts
+  # Toda la base de usuarios se hizo solo ejecutando rails generate scaffold User name:string email:string
+  #scaffold
+
+  #Note that there is no need to include a parameter for id; it is created automatically by Rails for use as the primary key in the database.)
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
 end
